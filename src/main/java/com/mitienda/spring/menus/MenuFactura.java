ee4b831dfcfca59f5c1c3b46dff3dc423eb3d7c7
SaRaPaMu <santi.rpm@gmail.com>
2019-12-01 21:30:42 +0100

    .

---
+++ src/main/java/com/mitienda/spring/menus/MenuFactura.java
@@ -0,0 +1,38 @@
+package com.mitienda.spring.menus;
+
+import java.util.Scanner;
+
+public class MenuFactura {
+	public void initMenuFactura() {
+
+		Scanner scan = new Scanner(System.in);
+		int numero;
+
+		System.out.println("\n");
+		System.out.println("===FACTURAS===\n");
+
+		System.out.println("(1).Crear\n");
+		System.out.println("(2).Actualizar\n");
+		System.out.println("(3).Consultar\n");
+		System.out.println("(4).Borrar\n");
+		System.out.println("(5).Listar\n");
+
+		System.out.println("Escoge una opcion: ");
+
+		numero = (int) Integer.parseInt(scan.nextLine());
+
+		switch (numero) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		}
+
+	}
+}
